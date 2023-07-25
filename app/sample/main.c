479be65e47f9c00da2ac882a64f8ee02277b9fd2
Jun-Ki, Hong <dylanhong920509@gmail.com>
2023-07-26 00:05:42 +0900

    build elf ok

--- app/sample/main.c
+++ app/sample/main.c
@@ -1,4 +1,8 @@
+#include <stdio.h>
+
 int main(void)
 {
+    printf("hello sample1!\n");
+    
     return 0;
 }
--- scripts/build.mk
+++ scripts/build.mk
@@ -1,6 +1,7 @@
 include $(scriptdir)/gcc.mk
 include $(subdir)/Makefile
 
+basedir ?=
 app := $(addsuffix .elf,$(app-y))
 lib := $(addsuffix .a,$(lib-y))
 drv := $(addsuffix .a,$(drv-y))
@@ -14,12 +15,17 @@ all: $(target)
 phony += $(app)
 %.elf:
 	$(Q)mkdir -p $(builddir)/$(subdir)/$(basename $@)
-	$(MAKE) -f $(scriptdir)/build.mk subdir=$(subdir)/$(basename $@) basedir=$(subdir)/$(basename $@)
+	$(Q)rm -f $(builddir)/$(subdir)/$(basename $@)/tmp.txt
+	$(MAKE) -f $(scriptdir)/build.mk subdir=$(subdir)/$(basename $@) basedir=$(builddir)/$(subdir)/$(basename $@)
+	$(Q)sort $(builddir)/$(subdir)/$(basename $@)/tmp.txt > $(builddir)/$(subdir)/$(basename $@)/objects.mk
+	$(MAKE) -f $(scriptdir)/Makefile target=$(builddir)/$(subdir)/$(basename $@)/$@ objects=$(builddir)/$(subdir)/$(basename $@)/objects.mk
 
 phony += $(lib)
 %.a:
 	$(Q)mkdir -p $(builddir)/$(subdir)/$(basename $@)
-	$(MAKE) -f $(scriptdir)/build.mk subdir=$(subdir)/$(basename $@) basedir=$(subdir)/$(basename $@)
+	$(Q)rm -f $(builddir)/$(subdir)/$(basename $@)/tmp.txt
+	$(MAKE) -f $(scriptdir)/build.mk subdir=$(subdir)/$(basename $@) basedir=$(builddir)/$(subdir)/$(basename $@)
+	$(Q)sort $(builddir)/$(subdir)/$(basename $@)/tmp.txt > $(builddir)/$(subdir)/$(basename $@)/objects.mk
 
 phony += $(dir)
 %.dir:
@@ -28,15 +34,19 @@ phony += $(dir)
 
 $(builddir)/%.o: %.c
 	$(CC) -c $< -o $@
+	@echo "obj += $@" >> $(basedir)/tmp.txt
 
 $(builddir)/%.o: %.cpp
 	$(CC) -c $< -o $@
+	@echo "obj += $@" >> $(basedir)/tmp.txt
 
 $(builddir)/%.o: %.S
 	$(CC) -c $< -o $@
+	@echo "obj += $@" >> $(basedir)/tmp.txt
 
 $(builddir)/%.lds: %.lds.S
 	$(CC) -c $< -o $@
+	@echo "lds += $@" >> $(basedir)/tmp.txt
 
 info:
 	$(Q)echo "topdir   : $(topdir)"
